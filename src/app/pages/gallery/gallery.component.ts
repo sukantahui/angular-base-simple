7541dd5799327b076f72d4ed9736a60e5e4214b4
sukanta hui <huisukanta77@gmail.com>
2020-07-01 11:30:13 +0530

    carousel Added

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -5,20 +5,20 @@ import { map } from 'rxjs/operators';
 
 const data = [
   {
-    srcUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg',
-    previewUrl: 'https://preview.ibb.co/jrsA6R/img12.jpg'
+    srcUrl: 'https://live.staticflickr.com/65535/49913013501_fab3b1f241_k.jpg',
+    previewUrl: 'https://live.staticflickr.com/65535/49913013501_a3c359f98b_t.jpg'
   },
   {
-    srcUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg',
-    previewUrl: 'https://preview.ibb.co/kPE1D6/clouds.jpg'
+    srcUrl: 'https://live.staticflickr.com/4870/45885586705_313ca01433_k.jpg',
+    previewUrl: 'https://live.staticflickr.com/4870/45885586705_63eeeff55e_t.jpg'
   },
   {
-    srcUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg',
-    previewUrl: 'https://preview.ibb.co/mwsA6R/img7.jpg'
+    srcUrl: 'https://live.staticflickr.com/7848/46010270344_522a57b03f_k.jpg',
+    previewUrl: 'https://live.staticflickr.com/7848/46010270344_8d03716eba_t.jpg'
   },
   {
-    srcUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg',
-    previewUrl: 'https://preview.ibb.co/kZGsLm/img8.jpg'
+    srcUrl: 'https://live.staticflickr.com/4816/32859915328_c5d34eac5a_k.jpg',
+    previewUrl: 'https://live.staticflickr.com/4816/32859915328_35525369b3_t.jpg'
   },
   {
     srcUrl: 'https://live.staticflickr.com/65535/49912494543_cac063d0da_k.jpg',
--- src/app/pages/home/picture-carousel/picture-carousel.component.ts
+++ src/app/pages/home/picture-carousel/picture-carousel.component.ts
@@ -1,5 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @Component({
@@ -7,13 +8,14 @@ import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/n
   templateUrl: './picture-carousel.component.html',
   styleUrls: ['./picture-carousel.component.scss']
 })
-export class PictureCarouselComponent {
-  images = [1, 2, 3, 4].map((n) => `assets/carousel/carousel_${n}.jpg`);
+export class PictureCarouselComponent implements OnInit{
+  images = [1, 2, 3, 4].map((n) => ({url: `assets/carousel/carousel_${n}.jpg`, caption: 'My Caption'}));
   pictureDetails: string[] = ['Beautiful Paro', 'I love nature', 'India is great', 'Stay at Home'];
   paused = false;
   unpauseOnArrow = false;
   pauseOnIndicator = false;
   pauseOnHover = true;
+  imageLinks: any;
 
   @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
 
@@ -36,4 +38,50 @@ export class PictureCarouselComponent {
     }
   }
 
+  ngOnInit(): void {
+    this.imageLinks = [
+      {
+        srcUrl: 'https://live.staticflickr.com/65535/49913013501_fab3b1f241_k.jpg',
+        previewUrl: 'https://live.staticflickr.com/65535/49913013501_a3c359f98b_t.jpg',
+        caption: '1 Image'
+      },
+      {
+        srcUrl: 'https://live.staticflickr.com/4870/45885586705_313ca01433_k.jpg',
+        previewUrl: 'https://live.staticflickr.com/4870/45885586705_63eeeff55e_t.jpg',
+        caption: '2 Image'
+      },
+      {
+        srcUrl: 'https://live.staticflickr.com/7848/46010270344_522a57b03f_k.jpg',
+        previewUrl: 'https://live.staticflickr.com/7848/46010270344_8d03716eba_t.jpg',
+        caption: '3 Image'
+      },
+      {
+        srcUrl: 'https://live.staticflickr.com/4816/32859915328_c5d34eac5a_k.jpg',
+        previewUrl: 'https://live.staticflickr.com/4816/32859915328_35525369b3_t.jpg',
+        caption: '4 Image'
+      },
+      {
+        srcUrl: 'https://live.staticflickr.com/65535/49912494543_cac063d0da_k.jpg',
+        previewUrl: 'https://live.staticflickr.com/65535/49912494543_c37c3fdf30_t.jpg'
+      },
+      {
+        srcUrl: 'https://live.staticflickr.com/65535/49913016581_144745fec5_k.jpg',
+        previewUrl: 'https://live.staticflickr.com/65535/49913016581_ecd7b57176_t.jpg',
+        caption: '5 Image'
+      },
+      {
+        srcUrl: 'https://live.staticflickr.com/65535/49913307822_357ec25f1d_k.jpg',
+        previewUrl: 'https://live.staticflickr.com/65535/49913307822_d2ef6a394f_t.jpg',
+        caption: '6 Image'
+      },
+      {
+        srcUrl: 'https://live.staticflickr.com/65535/49913309312_2bb1060a3a_k.jpg',
+        previewUrl: 'https://live.staticflickr.com/65535/49913309312_5694ed71d8_t.jpg',
+        caption: '7 Image'
+      }
+    ];
+  }
+
 }
+
+
